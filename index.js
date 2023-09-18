0f53e32baa196a2b75a665a6b5b0c02accdd666d
Nadim-001 <mdnadimahmed.001@gmail.com>
2023-09-18 15:37:29 +0100

    Connected HTML buttons to JS

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const submitBtn = document.querySelector("#form-btn");
-console.log(document.getElementById("form-btn"));
+const worldTourBtn = document.querySelector("#world-tour");
+const timeChallengeBtn = document.querySelector("#time-challenge");
+const form = document.querySelector("#user-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e.target.formName.value);
+});
+//event listener on form not button
