b05d71c1104682e7027dc83b1ef545343d8e0d19
Nadim-001 <mdnadimahmed.001@gmail.com>
2023-09-19 14:57:32 +0100

    Fixed submit button in Start page

--- GeoQuest/front-end/index.js
+++ GeoQuest/front-end/index.js
@@ -2,8 +2,21 @@ const worldTourBtn = document.querySelector("#world-tour");
 const timeChallengeBtn = document.querySelector("#time-challenge");
 const form = document.querySelector("#user-form");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log(e.target.formName.value);
-});
+const page = document.body.id;
+
+let testName;
+console.log("hello", page);
+
+if (page == "start") {
+  console.log("begin");
+  form.addEventListener("submit", (e) => {
+    testName = e.target.formName.value;
+  });
+}
+
 //event listener on form not button
+//got rid of the method="POST". the e.preventDefault was the issue
+
+document.addEventListener("dblclick", () => {
+  console.log(testName);
+});
